c6b546fcb402d506de898d488839130d1a738a21
BaybusinovV <vova_220403@mail.ru>
2024-05-28 22:50:29 +0500

    Part 12

--- pages/filter_2.py
+++ pages/filter_2.py
@@ -4,11 +4,11 @@ from time import sleep
 from pages.base import BasePage
 from selenium.webdriver.common.by import By
 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 plus_ingredients = (By.XPATH, "//div[@class='emotion-1wxt7g0']" and "//button[contains(text(), 'Куриное филе')]")
-
+add_ingredients = (By.XPATH, "//input[@type='text']")
+ingreditent = (By.XPATH, "//button[@class='emotion-kuminy']")
 
 class FiltersPage2(BasePage):
     def __init__(self, browser):
@@ -19,11 +19,38 @@ class FiltersPage2(BasePage):
     def open(self):
         self.browser.get('https://eda.ru')
         self.browser.refresh()
-
+################################################################
     def open_search(self):
         self.find((By.XPATH, "//span[text()='Ингредиенты, детали']")).click()
         sleep(2)
 
+    def add_ingred_find(self):
+        self.browser.implicitly_wait(10)
+        return self.find(add_ingredients)
+
+    def add_ingredients_click(self):
+        return self.add_ingred_find().click()
+
+    def add_ingredients_send_text(self):
+        return self.add_ingred_find().send_keys('Помидоры')
+
+    def find_ingredient_dropdown(self):
+        return self.find(ingreditent)
+
+    def choose_ingredient(self):
+        return self.find_ingredient_dropdown().click()
+################################################################
+    def minus_ingridients_find(self):
+        self.browser.implicitly_wait(10)
+        return self.find(minus_ingridients_find)
+
+    def minus(self):
+        self.minus_ingredients.click()
+        self.browser.implicitly_wait(10)
+
+
+
+################################################################
     def find_plus(self):
         self.plus_ingredients = self.find(plus_ingredients)
         self.browser.implicitly_wait(10)
@@ -39,5 +66,4 @@ class FiltersPage2(BasePage):
 
     def search(self):
         self.find((By.XPATH, "//button[@class='emotion-sjbikb']")).click()
-
-
+################################################################
--- tests/test_filter_2.py
+++ tests/test_filter_2.py
@@ -13,10 +13,25 @@ def test_filter_2(browser):
 
     with allure.step("open_search"):
         filters_page_2.open_search()
-
+################################################
     with allure.step("find_plus"):
         filters_page_2.find_plus()
+        sleep(2)
 
+    with allure.step("find_plus2"):
+        filters_page_2.add_ingredients_click()
+        sleep(2)
+
+    with allure.step("find_plus3"):
+        filters_page_2.add_ingredients_send_text()
+        sleep(2)
+
+    with allure.step("find_plus3"):
+        filters_page_2.choose_ingredient()
+        sleep(2)
+
+
+################################################
     with allure.step("plus"):
         filters_page_2.plus()
 
@@ -29,4 +44,6 @@ def test_filter_2(browser):
     with allure.step("search"):
         filters_page_2.search()
         sleep(2)
+
+
 
