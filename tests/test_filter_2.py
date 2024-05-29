789b04656e719beca1c7b8efcc875b283379ff35
Админ <baymir.rezerv@mail.ru>
2024-05-29 16:26:20 +0500

    Part-12.1

--- tests/test_filter_2.py
+++ tests/test_filter_2.py
@@ -13,37 +13,97 @@ def test_filter_2(browser):
 
     with allure.step("open_search"):
         filters_page_2.open_search()
-################################################
-    with allure.step("find_plus"):
-        filters_page_2.find_plus()
-        sleep(2)
+    ################################################
 
-    with allure.step("find_plus2"):
+    with allure.step("find_plus1_tomatos"):
         filters_page_2.add_ingredients_click()
         sleep(2)
 
-    with allure.step("find_plus3"):
+    with allure.step("find_plus2_tomatos"):
         filters_page_2.add_ingredients_send_text()
         sleep(2)
 
-    with allure.step("find_plus3"):
+    with allure.step("find_plus3_tomatos"):
         filters_page_2.choose_ingredient()
         sleep(2)
 
 
-################################################
-    with allure.step("plus"):
-        filters_page_2.plus()
+#
 
-    with allure.step("type"):
-        filters_page_2.type()
 
-    with allure.step("type2"):
-        filters_page_2.type()
+
+
+    ################################
+
+    with allure.step("find_plus1_cheese"):
+        filters_page_2.add_ingredients_click()
+        sleep(2)
+
+    with allure.step("find_plus2_cheese"):
+        filters_page_2.add_ingredients_send_text_2()
+        sleep(2)
+
+    with allure.step("find_plus3_cheese"):
+        filters_page_2.choose_ingredient()
+        sleep(2)
+
+    ################################################
+
+
+    with allure.step("del_ingredients_find"):
+        filters_page_2.del_ingredients_find()
+
+    with allure.step("del_ingredients_click"):
+        filters_page_2.del_ingredients_click()
+
+
+    with allure.step("del_ingredients_send_text"):
+        filters_page_2.del_ingredients_send_text()
+
+
+    with allure.step("del_ingredients_choose_ingredient"):
+        filters_page_2.del_choose_ingredient()
+
+
+        #################################################
+    with allure.step("del_ingredients_find_limon"):
+        filters_page_2.del_ingredients_find()
+
+    with allure.step("del_ingredients_click_limon"):
+        filters_page_2.del_ingredients_click()
+
+
+    with allure.step("del_ingredients_send_text_2"):
+        filters_page_2.del_ingredients_send_text_2()
+
+
+    with allure.step("del_ingredients_choose_ingredient_limon"):
+        filters_page_2.del_choose_ingredient()
+
+    #######################
+
+    with allure.step("popular_ingredients_find"):
+        filters_page_2.popular_ingredients_find()
+
+    with allure.step("popular_ingredients_click"):
+        filters_page_2.popular_ingredients_click()
+
+    with allure.step("type_find"):
+        sleep(1)
+        filters_page_2.type_find()
+
+    with allure.step("type_click"):
+        sleep(1)
+        filters_page_2.type_click()
+
+    with allure.step("type2_find"):
+        sleep(1)
+        filters_page_2.type_find()
+
+    with allure.step("type2_click"):
+        sleep(1)
+        filters_page_2.type_click()
 
     with allure.step("search"):
         filters_page_2.search()
         sleep(2)
-
-
-
